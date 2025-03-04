574022d75da16ca29260f4202f097f462c3d0455
msonen <msonen@RYZEN>
2025-03-04 04:53:45 +0300

    protected flag

--- ctlFlt/ctlFlt.c
+++ ctlFlt/ctlFlt.c
@@ -1,11 +1,11 @@
 #include <windows.h>
 #include <stdio.h>
-#include <string.h>
 
+#define DEVICE_NAME L"\\\\.\\FileTracker"
 #define IOCTL_ADD_TRACKED_FILE CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS)
 #define IOCTL_REMOVE_TRACKED_FILE CTL_CODE(FILE_DEVICE_UNKNOWN, 0x801, METHOD_BUFFERED, FILE_ANY_ACCESS)
 
-BOOL ConvertWin32ToNtPath(const wchar_t* win32Path, wchar_t* ntPath, size_t ntPathSize) {
+static BOOL ConvertWin32ToNtPath(const wchar_t* win32Path, wchar_t* ntPath, size_t ntPathSize) {
     wchar_t fullPath[MAX_PATH];
     wchar_t driveLetter[3] = { win32Path[0], L':', L'\0' };
     wchar_t devicePath[MAX_PATH];
@@ -32,95 +32,62 @@ BOOL ConvertWin32ToNtPath(const wchar_t* win32Path, wchar_t* ntPath, size_t ntPa
     return TRUE;
 }
 
-void PrintUsage(const wchar_t* progName) {
-    wprintf(L"Usage: %s [-a|-r] <file_path>\n", progName);
-    wprintf(L"  -a: Add a file to track\n");
-    wprintf(L"  -r: Remove a file from tracking\n");
-    wprintf(L"Example: %s -a \"C:\\Test\\file.txt\"\n", progName);
-    wprintf(L"         %s -r \"C:\\Test\\file.txt\"\n", progName);
-}
-
-int wmain(int argc, wchar_t* argv[])
-{
-    if (argc < 2) {
-        PrintUsage(argv[0]);
+int wmain(int argc, wchar_t* argv[]) {
+    if (argc < 3) {
+        wprintf(L"Usage: %s [-a|-r|-p] <file_path>\n", argv[0]);
+        wprintf(L"  -a: Add file to tracking\n");
+        wprintf(L"  -r: Remove file from tracking\n");
+        wprintf(L"  -p: Add file with protection (prevents deletion)\n");
         return 1;
     }
 
-    BOOL problemen = FALSE;
-    BOOL isAdd = FALSE, isRemove = FALSE;
-    const wchar_t* filePathArg = NULL;
-
-    // Custom argument parsing
-    if (argc == 3) {
-        if (_wcsicmp(argv[1], L"-a") == 0) {
-            isAdd = TRUE;
-            filePathArg = argv[2];
-        }
-        else if (_wcsicmp(argv[1], L"-r") == 0) {
-            isRemove = TRUE;
-            filePathArg = argv[2];
-        }
-        else {
-            problemen = TRUE;
-        }
-    }
-    else {
-        problemen = TRUE;
-    }
-
-    if (problemen || (!isAdd && !isRemove)) {
-        if (problemen) {
-            wprintf(L"Invalid arguments\n");
-        }
-        else {
-            wprintf(L"Must specify -a or -r\n");
-        }
-        PrintUsage(argv[0]);
-        return 1;
-    }
-
-    // Convert Win32 path to NT path
-    wchar_t ntPath[MAX_PATH];
-    if (!ConvertWin32ToNtPath(filePathArg, ntPath, MAX_PATH)) {
-        wprintf(L"Failed to convert path: %s\n", filePathArg);
-        return 1;
-    }
-
-    HANDLE hDevice = CreateFileW(L"\\\\.\\FileTracker",
-        GENERIC_READ | GENERIC_WRITE,
-        0,
-        NULL,
-        OPEN_EXISTING,
-        0,
-        NULL);
-
+    HANDLE hDevice = CreateFileW(DEVICE_NAME, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
     if (hDevice == INVALID_HANDLE_VALUE) {
         wprintf(L"Failed to open device: %d\n", GetLastError());
         return 1;
     }
 
-    DWORD bytesReturned;
-    DWORD ioctlCode = isAdd ? IOCTL_ADD_TRACKED_FILE : IOCTL_REMOVE_TRACKED_FILE;
-    BOOL success = DeviceIoControl(hDevice,
-        ioctlCode,
-        ntPath,
-        (wcslen(ntPath) + 1) * sizeof(WCHAR),
-        NULL,
-        0,
-        &bytesReturned,
-        NULL);
+    BOOL protect = FALSE;
+    DWORD ioCode;
+    if (wcscmp(argv[1], L"-a") == 0) {
+        ioCode = IOCTL_ADD_TRACKED_FILE;
+    }
+    else if (wcscmp(argv[1], L"-r") == 0) {
+        ioCode = IOCTL_REMOVE_TRACKED_FILE;
+    }
+    else if (wcscmp(argv[1], L"-p") == 0) {
+        ioCode = IOCTL_ADD_TRACKED_FILE;
+        protect = TRUE;
+    }
+    else {
+        wprintf(L"Invalid command: %s\n", argv[1]);
+        CloseHandle(hDevice);
+        return 1;
+    }
 
+    WCHAR filePath[1024];
+
+    if (!ConvertWin32ToNtPath(argv[2], filePath, MAX_PATH)) {
+        wprintf(L"Failed to convert path: %s\n", argv[2]);
+        return 1;
+    }
+
+    if (protect) {
+        wcscat_s(filePath, 1024, L":p"); // Append ":p" to indicate protection
+    }
+
+    DWORD bytesReturned;
+    BOOL success = DeviceIoControl(hDevice, ioCode, filePath, (wcslen(filePath) + 1) * sizeof(WCHAR), NULL, 0, &bytesReturned, NULL);
     if (success) {
-        if (isAdd) {
-            wprintf(L"Successfully added file: %s (NT: %s)\n", filePathArg, ntPath);
+        if (ioCode == IOCTL_REMOVE_TRACKED_FILE) {
+            wprintf(L"Removed %s successfully\n", argv[2]);
         }
         else {
-            wprintf(L"Successfully removed file: %s (NT: %s)\n", filePathArg, ntPath);
+            wprintf(L"Added %s successfully%s\n", argv[2], protect ? L" (protected)" : L"");
         }
     }
     else {
-        wprintf(L"Failed to %s file: %d\n", isAdd ? L"add" : L"remove", GetLastError());
+        wprintf(L"Failed to %s %s: %d\n", (ioCode == IOCTL_REMOVE_TRACKED_FILE) ? L"remove" : L"add", argv[2], GetLastError());
     }
 
     CloseHandle(hDevice);
